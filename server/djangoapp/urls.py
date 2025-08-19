ee38a95586a4eeae87b22d722e0c9e65ca0f16bc
vandanapan <vandanap@skillup.online>
2025-08-19 03:59:58 -0400

    carmodel

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,9 +11,9 @@ path(route='register', view=views.registration, name='register'),
     # path for login
 path(route='login', view=views.login_user, name='login'),
 path(route='logout', view=views.logout_request, name='logout'),  
-
+path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
-
+#path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
